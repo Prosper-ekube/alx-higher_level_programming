74a1ab7346954ba0740fea257e17d64ddf18c579
Prosper-ekube <prosperekube@gmail.com>
2023-03-12 04:01:08 -0700

    Quick fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def to_upper(char):
     if ord(char) >= 97 and ord(char) <= 122:
-        return  chr(ord(char) - 32)
+        return chr(ord(char) - 32)
     else:
         return chr(ord(char))
 
