0886e3a7737c15023152005afeb9856343f2e7e0
Prosper-ekube <prosperekube@gmail.com>
2023-03-13 17:54:15 -0700

    Quick fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
+    elif my_list != "":
+        biggestInt = my_list[0]
+        for i in range(len(my_list)):
+            if biggestInt < my_list[i]:
+                biggestInt = my_list[i]
+
+    return biggestInt
+                
