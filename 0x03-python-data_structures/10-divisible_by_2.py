411d28061e3fcac88dc438952c150bf014df7e25
Prosper-ekube <prosperekube@gmail.com>
2023-03-13 18:14:37 -0700

    Quick fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-def max_integer(my_list=[]):
-    if my_list == "":
-        return None
-    elif my_list != "":
-        biggestInt = my_list[0]
-        for i in range(len(my_list)):
-            if biggestInt < my_list[i]:
-                biggestInt = my_list[i]
+def divisible_by_2(my_list=[]):
 
-    return biggestInt
-                
+    newList = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            newList.append(1)
+        else:
+            newList.append(0)
+
+    return newList
