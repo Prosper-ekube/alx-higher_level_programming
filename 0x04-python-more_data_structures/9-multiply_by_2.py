6b13fdf2495eafca52c0a3f4b9de11da44ac6bc1
Prosper-ekube <prosperekube@gmail.com>
2023-03-17 10:18:53 -0700

    Quick fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
+    multipliedBy2 = 2
+    for i in a_dictionary.value():
+        multipliedBy2 *= i:
+            print(multipliedBy2)
+
+    return multipliedBy2
