79635921c08a4ad9fb999b3459a12f2aac3f5e98
Prosper-ekube <prosperekube@gmail.com>
2023-03-17 03:33:45 -0700

    Quick fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     newList = my_list[:]
-    for i in range (len(newList)):
-        if newList[i] == search:
+    for i in range(len(newList)):
+        if newList[i] == searchi:
             newList[i] = replace
     return newList
