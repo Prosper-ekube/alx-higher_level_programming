e129fb061f13d5a3e7a43bb99762485c1798a565
Prosper-ekube <prosperekube@gmail.com>
2023-03-18 05:39:37 -0700

    Quick fix

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    simpleDelete = a_dictionary.pop(key)
-    return simpleDelete
+    if key in a_dictionary:
+        del a_dictionary[key]
