dbde2b1c7303dfe1847475d3df032ce5228f5889
Prosper-ekube <prosperekube@gmail.com>
2023-03-21 11:41:14 -0700

    Quick fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,9 @@
 
 def print_last_digit(number):
     last_digit = abs(number) % 10
-    print(last_digit)
-    return last_digit
+    if number < 0 and last_digit != 0:
+        print(f"-{last_digit}")
+    elif number < 0 and last_digit == 0:
+        print(last_digit)
+    else:
+        print(last_digit)
