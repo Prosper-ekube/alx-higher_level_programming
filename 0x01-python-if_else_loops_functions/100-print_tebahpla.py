431f5b38609884e69f05392ac0b7f5f6601e6b30
Prosper-ekube <prosperekube@gmail.com>
2023-03-24 21:27:11 -0700

    refactor: refactor 100-print_tebahpla.py
    use string format, thereby shortening the codebase.

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 
-for i in range(90, 64, -1):
-    if i % 2 == 0:
-        print(chr(i + 32), end="")
-    else:
-        print(chr(i), end="")
+print(''.join(chr(i + 32) if i % 2 == 0 else chr(i) for i in range(90, 64, -1)))
