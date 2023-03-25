e07bd029fd5d54bcb91adf42eba489755eececfa
Prosper-ekube <prosperekube@gmail.com>
2023-03-25 06:06:38 -0700

    chore: add code to 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,2 @@
 #!/usr/bin/python3
 
-output = ""
-
-for i in range(90, 64, -1):
-    if i % 2 == 0:
-        output += "{}".format(chr(i + 32))
-    else:
-        output += "{}".format(chr(i))
-print("{}".format(output))
