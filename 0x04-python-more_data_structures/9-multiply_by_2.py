aa786c178c7707aeb46ab9bc117b0098335f2d48
Prosper-ekube <prosperekube@gmail.com>
2023-03-26 11:20:44 -0700

    refactor: change codebase to suit requirements

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-def multiply_by_2(a_dictionary):
-    multipliedBy2 = 2
-    for i in a_dictionary.value():
-        multipliedBy2 *= i:
-            print(multipliedBy2)
-
-    return multipliedBy2
+def multiply_by_2(my_dict):
+    return {key: value * 2 for key, value in my_dict.items()}
