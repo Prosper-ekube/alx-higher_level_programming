edc65c95a3c9a0cbdc87900ce3fffd460a4b744c
Prosper-ekube <prosperekube@gmail.com>
2023-03-26 12:45:21 -0700

    Quick fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(122, 64, -1):
-    print("{:c}".format(i).upper() if i % 2 == 0
-          else "{:c}".format(i).lower(), end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
