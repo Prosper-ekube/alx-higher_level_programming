80f3eba1e6417819497b6a0d5d824553ae9dbd3a
Prosper-ekube <prosperekube@gmail.com>
2023-03-26 12:29:24 -0700

    Quick fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        print(f"{len(args)} arguments.")
+    else:
+        arg_position = args.index(args[0])
+        arg_value = args[arg_position]
+        num_args = len(args)
+        print(f"{num_args} argument:")
+        print(f"{arg_position}: {arg_value}")
