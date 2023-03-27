df3153778e10e6785bfdc77bb785214d95d1a603
Prosper-ekube <prosperekube@gmail.com>
2023-03-27 08:24:07 -0700

    refactor: refactor codebase

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import sys
-    i = len(sys.argv) - 1
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        print(f"{len(args)} arguments.")
     else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        num_args = len(args)
+        if num_args == 1:
+            print(f"{num_args} argument:")
+        else:
+            print(f"{num_args} arguments:")
+        for i in args:
+            arg_position = args.index(i)
+            arg_value = args[arg_position]
+            print(f"{arg_position + 1}: {arg_value}")
