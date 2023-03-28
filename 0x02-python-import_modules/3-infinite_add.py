13b6302df3f717dd8fb075da435b238ee3336c2a
Prosper-ekube <prosperekube@gmail.com>
2023-03-28 05:15:16 -0700

    refactor: changed codebase to suit my requirements

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    
+    args = sys.argv[1:]
+    summm = 0
+
+    for i in args:
+        arg_position = args.index(i)
+        arg_value = args[arg_position]
+        x = int(arg_value)
+        summm += x
+
+    print(summm)
