44ec119d3c06135f5178064c5238e22a38bf7462
Prosper-ekube <prosperekube@gmail.com>
2023-04-05 16:34:06 -0700

    Add 5-rectangle.py Add 6-rectangle.py

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Defines a class of name Rectangle"""
+
+
+class Rectangle:
+    """Initializes the class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.__height = 0
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width attruibute"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height attribute"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be <= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """"Returns the area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return ((self.width * 2) + (self.height * 2))
+
+    def __str__(self):
+        """Returns printable string representation of the rectangle"""
+        if self.__width != 0 and self.__height != 0:
+            return "\n".join(f"{'#' * self.__width}"
+                             for j in range(self.__height))
+        else:
+            return ""
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle for recreation"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Prints this when an instance of Rectangle is deleted"""
+        print("Bye rectangle...")
