a93040c2e5e0395d995e16a97f40d707771329f8
Prosper-ekube <prosperekube@gmail.com>
2023-04-11 12:16:02 -0700

    docs: add documentation to 0-lookup.py
    This adds documentation to 0-lookup.py within 0-lookup.py.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
+
+"""Defines a function (lookup)"""
+
+
 def lookup(obj):
+    """This function returns the list of available
+    attributes and methods of an object"""
     return dir(obj)
